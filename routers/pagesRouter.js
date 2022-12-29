e38380826faa6750a79474581f47da49a9765697
Alibi Duisenaliyev <realibi.dev@gmail.com>
2022-12-29 19:28:10 +0600

    actualize

--- routers/pagesRouter.js
+++ routers/pagesRouter.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const fs = require("fs");
 const router = express.Router();
-const { COMPONENTS_FILE_PATH, getDataFromFile } = require("../utils.js");
+const { PAGES_FILE_PATH, getDataFromFile } = require("../utils.js");
 
 router.post("/", (req, res) => {
-    
+    const { pageName, componentsIds } = req.body;
+    const pages = getDataFromFile(PAGES_FILE_PATH);
 })
 
 module.exports = router;
--- utils.js
+++ utils.js
@@ -1,6 +1,15 @@
 const fs = require("fs");
 
 module.exports.COMPONENTS_FILE_PATH = "./components.json";
+module.exports.PAGES_FILE_PATH = "./pages.json";
 module.exports.getDataFromFile = (filePath) => {
-    return JSON.parse(fs.readFileSync(filePath, { encoding: "utf-8" }) || '[]');
+    const dataFromFile = [];
+
+    try {
+        dataFromFile = JSON.parse(fs.readFileSync(filePath, { encoding: "utf-8" }) || '[]');
+    } catch {
+        console.log('Error reading from file', filePath);
+    }
+
+    return dataFromFile;
 }
